d425c8c51ed3d702775181eb0fd248ccb38424a9
dat-do@cybozu.vn <d001524@pc-001524-3.local>
2022-04-23 14:08:08 +0700

    Initiate project

---
+++ wdio.conf.js
@@ -0,0 +1,25 @@
+exports.config = {
+    specs: [
+        './test/specs/**/*.js'
+    ],
+    exclude: [
+        // 'path/to/excluded/files'
+    ],
+    maxInstances: 10,
+    capabilities: [{
+        browserName: 'chrome',
+        acceptInsecureCerts: true
+    }],
+    logLevel: 'info',
+    bail: 0,
+    baseUrl: 'http://localhost',
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 120000,
+    connectionRetryCount: 3,
+    framework: 'mocha',
+    reporters: ['spec'],
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 60000
+    },
+}
